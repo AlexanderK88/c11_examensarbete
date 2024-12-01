c98181f3695008596af1d75aec011d9faae95ed3
Kookiie88 <alexander@jj-media.se>
2024-12-01 18:58:49 +0700

    Setup Project
    Co-authored-by: Weeezzt <Weeezzt@users.noreply.github.com>

---
+++ src/main/java/com/example/c11_examensarbete/C11ExamensarbeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.c11_examensarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C11ExamensarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C11ExamensarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/c11_examensarbete/C11ExamensarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c11_examensarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C11ExamensarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
